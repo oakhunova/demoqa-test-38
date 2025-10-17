8b2fbff2e74ac1a6f356c19693962ca31ad10450
Olga Akhunova <akhunovaolya7@rambler.ru>
2025-10-17 15:24:57 +0500

    renamed the method to fillFormTest added scripts to remove banner changed script for dropdown menu added jpg file added results checking

--- src/test/java/tests/QAGuruTest.java
+++ src/test/java/tests/QAGuruTest.java
@@ -4,12 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.ElementsCollection;
 
 public class QAGuruTest {
     @BeforeAll
-    static void beforeAll() {
+    static void setupEnvironment() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
@@ -21,6 +22,8 @@ public class QAGuruTest {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("test7@mail.com");
@@ -32,7 +35,6 @@ public class QAGuruTest {
         $(".react-datepicker__day--001").click();
         $("#subjectsInput").click();
         $("#subjectsInput").setValue("Arts");
-        $(".subjects-auto-complete__menu").find("#subjectsInput");
         $(".subjects-auto-complete__menu").click();
         $("label[for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFromClasspath("Cat.jpg");
@@ -43,5 +45,36 @@ public class QAGuruTest {
         $("#react-select-4-option-0").click();
         $("#submit").click();
 
+        ElementsCollection rows = $(".table-responsive").findAll("tr");
+        rows.findBy(text("Student Name"))
+                .findAll("td").get(1)
+                .shouldHave(text("Alex Ivanov"));
+        rows.findBy(text("Student Email"))
+                .findAll("td").get(1)
+                .shouldHave(text("test7@mail.com"));
+        rows.findBy(text("Gender"))
+                .findAll("td").get(1)
+                .shouldHave(text("Male"));
+        rows.findBy(text("Mobile"))
+                .findAll("td").get(1)
+                .shouldHave(text("1234567890"));
+        rows.findBy(text("Date of Birth"))
+                .findAll("td").get(1)
+                .shouldHave(text("01 January,1990"));
+        rows.findBy(text("Subjects"))
+                .findAll("td").get(1)
+                .shouldHave(text("Arts"));
+        rows.findBy(text("Hobbies"))
+                .findAll("td").get(1)
+                .shouldHave(text("Music"));
+        rows.findBy(text("Picture"))
+                .findAll("td").get(1)
+                .shouldHave(text("Cat.jpg"));
+        rows.findBy(text("Address"))
+                .findAll("td").get(1)
+                .shouldHave(text("Worldwide"));
+        rows.findBy(text("State and City"))
+                .findAll("td").get(1)
+                .shouldHave(text("NCR Delhi"));
     }
 }
