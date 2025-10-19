f5549367305e938c87b87c7bdd531270b390f8f0
Olga Akhunova <akhunovaolya7@rambler.ru>
2025-10-19 19:30:21 +0500

    added Page Objects, tests and components

---
+++ src/test/java/pages/components/NegativeTestCheckResultComponent.java
@@ -0,0 +1,10 @@
+package pages.components;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Selenide.$;
+
+public class NegativeTestCheckResultComponent {
+    public void negativeTestCheckResult() {
+        $("#userForm").shouldHave(cssClass("was-validated"));
+    }
+}
---
+++ src/test/java/tests/PracticeFormWithPageObjectsTests.java
@@ -0,0 +1,63 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.PracticeFormPage;
+
+public class PracticeFormWithPageObjectsTests extends TestBase{
+
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
+
+    @Test
+    void fillFormTest() {
+        practiceFormPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Ivanov")
+                .setUserEmail("test7@mail.com")
+                .setGender("Male")
+                .setUserPhone("1234567890")
+                .setBirthday("01", "January", "1990")
+                .setSubject("Arts")
+                .hobbyCheckbox("Music")
+                .uploadPicture("Cat.jpg")
+                .setAddress("Worldwide")
+                .stateDropdown()
+                .stateCityWrapper("NCR")
+                .cityDropdown()
+                .stateCityWrapper("Delhi")
+                .submitForm()
+                .verifyResult("Student Name", "Alex Ivanov")
+                .verifyResult("Student Email", "test7@mail.com")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "1234567890")
+                .verifyResult("Date of Birth", "01 January,1990")
+                .verifyResult("Subjects", "Arts")
+                .verifyResult("Hobbies", "Music")
+                .verifyResult("Picture", "Cat.jpg")
+                .verifyResult("Address", "Worldwide")
+                .verifyResult("State and City", "NCR Delhi");
+
+    }
+    @Test
+    void fillRequiredFieldsTest() {
+        practiceFormPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Ivanov")
+                .setGender("Male")
+                .setUserPhone("1234567890")
+                .submitForm()
+                .verifyResult("Student Name", "Alex Ivanov")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "1234567890");
+    }
+    @Test
+    void invalidMobileNumberTest() {
+        practiceFormPage.openPage()
+                .setFirstName("Alex")
+                .setLastName("Ivanov")
+                .setGender("Male")
+                .setUserPhone("123")
+                .submitForm()
+                .verifyResultForNegativeTest();
+    }
+}
+
